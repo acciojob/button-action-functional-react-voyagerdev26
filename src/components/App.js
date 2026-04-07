1e1d58d365f9739de839ce89817509bfd702c7bf
voyagerdev26 <devs251898@gmail.com>
2026-04-07 13:15:36 +0530

    version one

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,25 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+
+  let[showHide, setShowHide]= useState("disable");
+
+  function handleClick(e){
+    if(showHide==="disable"){
+
+      setShowHide("active");
+    }
+    else{
+      setShowHide("disable");
+
+    }
+  }
+
   return (
     <div id="main">
-      // Do not alter the main div
+      
+      <button id="click"  onClick={handleClick}>Click</button>
+      <p className={showHide} id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>
     </div>
   );
 }
