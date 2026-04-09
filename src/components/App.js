391417841bf44b00f487088413aab33f2afaf182
voyagerdev26 <devs251898@gmail.com>
2026-04-09 18:57:27 +0530

    version two

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,33 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  // 1st way 
+  // let[showHide, setShowHide]= useState("disable");
 
-  let[showHide, setShowHide]= useState("disable");
-
+  // 2nd way 
+  let[text, setText]= useState(false);
   function handleClick(e){
-    if(showHide==="disable"){
+    // 1st way
+    // if(showHide==="disable"){
 
-      setShowHide("active");
-    }
-    else{
-      setShowHide("disable");
+    //   setShowHide("active");
+    // }
+    // else{
+    //   setShowHide("disable");
 
-    }
+    // }
+    // 2nd way 
+
+    setText(!text);
   }
 
   return (
     <div id="main">
       
       <button id="click"  onClick={handleClick}>Click</button>
-      <p className={showHide} id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>
+      
+      {/* <p className={showHide} id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p> */}
+      {text && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>}
     </div>
   );
 }
